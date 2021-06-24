76ca82e99442f013f14adae44375835599020527
Muskan Muskan <ubxtymuskan@gmail.com>
2021-06-24 11:09:46 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter30days/pages/day3.dart';
 import 'package:flutter30days/pages/day4.dart';
 import 'package:flutter30days/pages/day5.dart';
 import 'package:flutter30days/pages/day6.dart';
+import 'package:flutter30days/pages/day7.dart';
 import 'package:flutter30days/pages/home_page.dart';
 import 'package:flutter30days/pages/image_page.dart';
 import 'package:flutter30days/pages/login_page.dart';
@@ -20,9 +21,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      themeMode : ThemeMode.light,
+      darkTheme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: '/day7',
+      routes: {
+        '/':(context) => HomePage(),
+        '/login':(context) => LoginPage(),
+        '/signUpPage':(context) => SignUpPage(),
+        '/day7':(context) => Day7()
+      },
       debugShowCheckedModeBanner: false,
       title: 'flutter 30 days challenge',
-      home: MyStatefulWidget()
+      // home: MyStateFullWidget()
     );
   }
 }
---
+++ lib/pages/day7.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+
+class Day7 extends StatefulWidget {
+  const Day7({Key? key}) : super(key: key);
+
+  @override
+  _Day7State createState() => _Day7State();
+}
+
+class _Day7State extends State<Day7> {
+  String data = '';
+  double h = 100;
+  double w = 200;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              color: Colors.green,
+              width: w,
+              transform: Matrix4.rotationZ(h),
+              height: h,
+              curve: Curves.easeInCirc,
+              duration: Duration(milliseconds: 300),
+            ),
+            Text('data = $data'),
+            SizedBox(height: 20,),
+            InkWell(
+              onTap: () {
+                call();
+              },
+              child: Container(
+                height: 50,
+                width: 250,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                ),
+                child: Text('Click me'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  void call() {
+    setState(() {
+      h+=50;
+      w+=50;
+      data = 'call method execute, 2nd calculate ${calculate(10, 20)}';
+    });
+  }
+  int calculate(int num1, int num2) {
+  return num1 + num2;
+  }
+
+}
